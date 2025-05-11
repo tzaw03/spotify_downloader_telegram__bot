9229b9ce6910de08579a462b71fc7ee4720bf60f
tzaw03 <128713204+tzaw03@users.noreply.github.com>
2025-05-11 14:56:52 +0700

    Update main.py
    change main

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import asyncio
 # Add the /app directory to sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from telegram import BOT_TOKEN, CLIENT, album_callback_query, new_message, artist_callback_query, playlist_callback_query
+from telegram import BOT_TOKEN, CLIENT, song_callback_query, album_callback_query, new_message, artist_callback_query, playlist_callback_query
 
 async def main():
     print('[BOT] Starting...')
     await CLIENT.start(bot_token=BOT_TOKEN)
+    song_callback_query.register_callbacks(CLIENT)
     album_callback_query.register_callbacks(CLIENT)
     artist_callback_query.register_callbacks(CLIENT)
     playlist_callback_query.register_callbacks(CLIENT)
